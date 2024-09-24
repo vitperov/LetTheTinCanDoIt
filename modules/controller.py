d1775dc644f38cfb4fb637fbae2291e3352486c0
Vitaly Perov <vitperov@gmail.com>
2024-09-24 23:21:14 +0300

    Call model methods directly

--- modules/controller.py
+++ modules/controller.py
@@ -1,11 +1,8 @@
-from PyQt5.QtCore import QObject, pyqtSignal
+from PyQt5.QtCore import QObject
 from modules.model.model import ProjectGPTModel
 from modules.view.view import ProjectGPTView
 
 class ProjectGPTController(QObject):
-    send_request = pyqtSignal(str, str, str)  # For single response: model, role_string, full request
-    send_batch_request = pyqtSignal(str, str, str)  # For batch response: model, role_string, full request
-
     def __init__(self):
         super().__init__()
         self.model = ProjectGPTModel()
@@ -15,32 +12,27 @@ class ProjectGPTController(QObject):
         self.view.send_request.connect(self.handle_send_request)
         self.view.send_batch_request.connect(self.handle_send_batch_request)
 
-        # Single response request
-        self.send_request.connect(self.model.generate_response)
-
-        # Batch response request
-        self.send_batch_request.connect(self.model.generate_batch_response)
-
+        # Connect the model's response generation signal to update the view
         self.model.response_generated.connect(self.view.update_response)
 
     def handle_send_request(self, model, role_string, full_request):
         """
         Handle single file request.
         """
-        # Call the set_project_files method
+        # Get the selected project directory and chosen files from the view
         project_dir, chosen_files = self.view.left_panel.get_checked_files()
         self.model.set_project_files(project_dir, chosen_files)
 
-        # Pass all received arguments to the model via the signal for a single response
-        self.send_request.emit(model, role_string, full_request)
+        # Directly call the model's method to generate a single response
+        self.model.generate_response(model, role_string, full_request)
 
     def handle_send_batch_request(self, model, role_string, full_request_template):
         """
         Handle batch request for multiple sets of files.
         """
-        # Call the set_project_files method
+        # Get the selected project directory and chosen files from the view
         project_dir, chosen_files = self.view.left_panel.get_checked_files()
         self.model.set_project_files(project_dir, chosen_files)
 
-        # Emit signal for batch response with multiple file groups
-        self.send_batch_request.emit(model, role_string, full_request_template)
+        # Directly call the model's method to generate a batch response
+        self.model.generate_batch_response(model, role_string, full_request_template)
